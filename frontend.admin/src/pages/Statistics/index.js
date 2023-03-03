270a2e2f546cea74f5df58d5d4cce7610da9e626
khanglb2000 <lambaokhang2000@gmail.com>
2023-03-03 19:56:35 +0700

    [MOD] Remove image in current list

--- frontend.admin/src/pages/Statistics/index.js
+++ frontend.admin/src/pages/Statistics/index.js
@@ -1,6 +1,6 @@
 import { faUpload } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Button, Spin, Upload } from 'antd';
+import { Button, Carousel, Spin, Upload } from 'antd';
 import {
   getDownloadURL,
   getMetadata,
@@ -8,44 +8,54 @@ import {
   uploadBytesResumable,
 } from 'firebase/storage';
 import { useState } from 'react';
+import { Card, Col, Image, Row } from 'react-bootstrap';
 import { toast } from 'react-toastify';
 import CustomModal from '~/components/Modal';
 import { storage } from '~/firebase';
 
+const contentStyle = {
+  color: '#fff',
+  textAlign: 'center',
+  height: 300,
+  background: '#364d79',
+};
+
 function Statistics() {
   const [fileList, setFileList] = useState([]);
+  const [backupFileList, setbackupFileList] = useState([]);
   const [productImages, setProductImages] = useState([]);
   const [show, setShow] = useState(false);
-  const [urls, setUrls] = useState([]);
+  const [urls, setUrls] = useState([
+    {
+      type: 'image/jpeg',
+      url: 'https://firebasestorage.googleapis.com/v0/b/bird-shop-22ade.appspot.com/o/productImages%2Fsinging-birds-banner-abstract-folk-bird-sitting-on-rope-isolated-decorative-animal-vector-element-2H48K82.jpg?alt=media&token=5ced7242-8cdb-4f09-af33-85a0c5b04976',
+    },
+  ]);
   const [progress, setProgress] = useState(0);
+  const [count, setCount] = useState(1);
 
   const fileListData = [
+    // Need convert firebase link to file
     {
       uid: '0',
-      name: 'sample-loading.png',
-      status: 'uploading',
-      percent: 33,
-    },
-    {
-      uid: '-1',
-      name: 'sample-bird.png',
+      name: 'bird-shop.png',
       status: 'done',
-      url: 'https://thanhphochomeo.b-cdn.net/wp-content/uploads/2019/12/chich-cheo-lua-2.png',
+      url: 'https://firebasestorage.googleapis.com/v0/b/bird-shop-22ade.appspot.com/o/productImages%2Fsinging-birds-banner-abstract-folk-bird-sitting-on-rope-isolated-decorative-animal-vector-element-2H48K82.jpg?alt=media&token=5ced7242-8cdb-4f09-af33-85a0c5b04976',
       thumbUrl:
-        'https://thanhphochomeo.b-cdn.net/wp-content/uploads/2019/12/chich-cheo-lua-2.png',
-    },
-    {
-      uid: '-2',
-      name: 'sample-error.png',
-      status: 'error',
+        'https://firebasestorage.googleapis.com/v0/b/bird-shop-22ade.appspot.com/o/productImages%2Fsinging-birds-banner-abstract-folk-bird-sitting-on-rope-isolated-decorative-animal-vector-element-2H48K82.jpg?alt=media&token=5ced7242-8cdb-4f09-af33-85a0c5b04976',
     },
   ];
 
   // Manage upload image action
-  const handleChangeProductImage = (file) => {
-    if (file) {
-      setProductImages((prevState) => [...prevState, file]);
+  const handleChangeProductImage = () => {
+    setUrls([]); // Reset urls list
+    console.log('fileList: ', fileList);
+    for (let i = 0; i < fileList.length; i++) {
+      if (fileList[i]) {
+        setProductImages((prevState) => [...prevState, fileList[i]]);
+      }
     }
+    setShow(false);
   };
 
   const handleUploadImage = () => {
@@ -81,34 +91,63 @@ function Statistics() {
     Promise.all(promises)
       .then(() => toast.success('Hình ảnh đã dược tải lên', { autoClose: 500 }))
       .catch((err) => console.log(err));
-
-    setShow(false);
   };
 
   console.log('urls: ', urls);
 
   const handleOpenImageModal = () => {
     setShow(true);
-    setFileList(fileListData);
+    if (count === 1) {
+      setFileList(fileListData);
+      setCount(0);
+    } else {
+      setFileList(backupFileList);
+    }
     setProductImages([]);
   };
 
   const handleCloseImageModal = () => {
     setShow(false);
-    setFileList([]);
     setProductImages([]);
   };
 
   return (
     <>
       <h2>Statistics Page</h2>
-      {/* <Button
+      <Card>
+        <Row>
+          <Col md={12}>
+            <Carousel autoplay>
+              {urls
+                ? urls.map((imageUrl, index) => {
+                    return (
+                      <div key={index}>
+                        <h3 style={contentStyle}>
+                          <Image
+                            src={imageUrl.url}
+                            style={{
+                              width: '100%',
+                              height: 'auto',
+                            }}
+                            alt="Ảnh"
+                          />
+                        </h3>
+                      </div>
+                    );
+                  })
+                : ''}
+            </Carousel>
+          </Col>
+        </Row>
+      </Card>
+
+      <Button
         variant="primary"
         onClick={handleUploadImage}
         disabled={productImages ? false : true}
       >
         Tải ảnh lên
-      </Button> */}
+      </Button>
       <Button variant="primary" onClick={handleOpenImageModal}>
         Chỉnh sửa danh sách ảnh
       </Button>
@@ -120,15 +159,29 @@ function Statistics() {
             <>
               <Upload.Dragger
                 multiple
-                //action={'http://localhost:3001/dashboard/statistics'}
                 listType="picture"
                 accept=".png,.jpg,.jpeg"
                 showUploadList={{ showRemoveIcon: true }}
                 defaultFileList={[...fileList]}
                 beforeUpload={(file) => {
-                  console.log(productImages);
-                  handleChangeProductImage(file);
+                  setbackupFileList((prevState) => [
+                    ...prevState,
+                    Object(file),
+                  ]);
+
+                  setFileList((prevState) => [...prevState, Object(file)]);
                   return false;
+                }}
+                onRemove={(file) => {
+                  const index = backupFileList.findIndex(
+                    (selectedFile) => selectedFile.name === file.name,
+                  );
+                  index !== -1 && backupFileList.splice(index, 1);
+
+                  const findIndex = fileList.findIndex(
+                    (selectedFile) => selectedFile.name === file.name,
+                  );
+                  findIndex !== -1 && fileList.splice(findIndex, 1);
                 }}
                 iconRender={() => {
                   return <Spin></Spin>;
@@ -153,7 +206,7 @@ function Statistics() {
             </>
           }
           handleClose={handleCloseImageModal}
-          handleSubmit={handleUploadImage}
+          handleSubmit={handleChangeProductImage}
         />
       </div>
     </>
