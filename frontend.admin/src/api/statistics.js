ec68b7529462f72a1102049489c7b61e63d3ba38
khanglb2000 <lambaokhang2000@gmail.com>
2023-03-13 07:12:53 +0700

    [MOD] Load store brief stats

--- frontend.admin/src/api/statistics.js
+++ frontend.admin/src/api/statistics.js
@@ -2,6 +2,24 @@ import api from './api';
 
 const statURL = 'stats';
 const city = 'cities';
+const briefRecord = 'counts';
+
+// Get store brief records
+export const getStoreBriefRecordsData = async () => {
+  try {
+    const response = await api.get(`/${statURL}/${briefRecord}`);
+    if (response.status !== 200) {
+      throw new Error('Store brief records has the problem');
+    }
+    const data = await response;
+    if (data) {
+      return data;
+    }
+    return false;
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 // Get all cities records
 export const getCitiesRecordsData = async () => {
--- frontend.admin/src/pages/Statistics/index.js
+++ frontend.admin/src/pages/Statistics/index.js
@@ -16,11 +16,31 @@ import PieChart from './Charts/PieChart';
 import LineChart from './Charts/LineChart';
 import BarChart from './Charts/BarChart';
 import './Statistics.scss';
+import { getStoreBriefRecordsData } from '~/api/statistics';
+import CustomSpinner from '~/ui/CustomSpinner';
 
 function Statistics() {
   const { RangePicker } = DatePicker;
+  const [brief, setBrief] = useState(null);
+  console.log('🚀 ~ file: index.js:24 ~ Statistics ~ brief:', brief);
+  const [loading, setLoading] = useState(true);
   const [startPeriod, setStartPeriod] = useState('');
   const [endPeriod, setEndPeriod] = useState('');
+
+  // Get Cities records
+  const getStoreBriefRecordList = useCallback(async () => {
+    try {
+      const data = await getStoreBriefRecordsData();
+      setBrief(data.data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    getStoreBriefRecordList();
+  }, [getStoreBriefRecordList]);
 
   //Choose date range
   const onDateSelection = (value, dateString) => {
@@ -39,119 +59,127 @@ function Statistics() {
 
   return (
     <>
-      <div style={{ textAlign: 'center' }}>
-        <h2>Thống kê</h2>
-      </div>
-      <div
-        style={{
-          marginTop: 20,
-          marginBottom: 20,
-          display: 'flex',
-          justifyContent: 'center',
-        }}
-      >
-        <span style={{ marginTop: 3, marginRight: 10, fontSize: '1.01rem' }}>
-          Chọn Ngày:
-        </span>
-        <RangePicker
-          placeholder={['Từ ngày', 'Đến ngày']}
-          format={defaultDatePickerRange}
-          onChange={onDateSelection}
-          disabledDate={disabledDate}
-          bordered="true"
-        />
-      </div>
-      <Row gutter={8}>
-        <Col span={6}>
-          <DashboardCard
-            icon={
-              <ShoppingCartOutlined
-                style={{
-                  color: 'green',
-                  backgroundColor: 'rgba(0,255,0,0.25)',
-                  borderRadius: '50%',
-                  fontSize: '2rem',
-                  padding: 12,
-                }}
+      {loading ? (
+        <CustomSpinner />
+      ) : (
+        <>
+          <div style={{ textAlign: 'center' }}>
+            <h2>Thống kê</h2>
+          </div>
+          <div
+            style={{
+              marginTop: 20,
+              marginBottom: 20,
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+          >
+            <span
+              style={{ marginTop: 3, marginRight: 10, fontSize: '1.01rem' }}
+            >
+              Chọn Ngày:
+            </span>
+            <RangePicker
+              placeholder={['Từ ngày', 'Đến ngày']}
+              format={defaultDatePickerRange}
+              onChange={onDateSelection}
+              disabledDate={disabledDate}
+              bordered="true"
+            />
+          </div>
+          <Row gutter={8}>
+            <Col span={6}>
+              <DashboardCard
+                icon={
+                  <ShoppingCartOutlined
+                    style={{
+                      color: 'green',
+                      backgroundColor: 'rgba(0,255,0,0.25)',
+                      borderRadius: '50%',
+                      fontSize: '2rem',
+                      padding: 12,
+                    }}
+                  />
+                }
+                title={'Đơn hàng'}
+                value={brief?.orders}
               />
-            }
-            title={'Đơn hàng'}
-            value={2345}
-          />
-        </Col>
-        <Col span={6}>
-          <DashboardCard
-            icon={
-              <ShoppingOutlined
-                style={{
-                  color: 'blue',
-                  backgroundColor: 'rgba(0,0,255,0.25)',
-                  borderRadius: '50%',
-                  fontSize: '2rem',
-                  padding: 12,
-                }}
+            </Col>
+            <Col span={6}>
+              <DashboardCard
+                icon={
+                  <ShoppingOutlined
+                    style={{
+                      color: 'blue',
+                      backgroundColor: 'rgba(0,0,255,0.25)',
+                      borderRadius: '50%',
+                      fontSize: '2rem',
+                      padding: 12,
+                    }}
+                  />
+                }
+                title={'Kho hàng'}
+                value={brief?.products}
               />
-            }
-            title={'Kho hàng'}
-            value={15000}
-          />
-        </Col>
-        <Col span={6}>
-          <DashboardCard
-            icon={
-              <UserOutlined
-                style={{
-                  color: 'purple',
-                  backgroundColor: 'rgba(0,255,255,0.25)',
-                  borderRadius: '50%',
-                  fontSize: '2rem',
-                  padding: 12,
-                }}
+            </Col>
+            <Col span={6}>
+              <DashboardCard
+                icon={
+                  <UserOutlined
+                    style={{
+                      color: 'purple',
+                      backgroundColor: 'rgba(0,255,255,0.25)',
+                      borderRadius: '50%',
+                      fontSize: '2rem',
+                      padding: 12,
+                    }}
+                  />
+                }
+                title={'Khách hàng'}
+                value={brief?.customers}
               />
-            }
-            title={'Khách hàng'}
-            value={50}
-          />
-        </Col>
-        <Col span={6}>
-          <DashboardCard
-            icon={
-              <DollarCircleOutlined
-                style={{
-                  color: 'red',
-                  backgroundColor: 'rgba(255,0,0,0.25)',
-                  borderRadius: '50%',
-                  fontSize: '2rem',
-                  padding: 12,
-                }}
+            </Col>
+            <Col span={6}>
+              <DashboardCard
+                icon={
+                  <DollarCircleOutlined
+                    style={{
+                      color: 'red',
+                      backgroundColor: 'rgba(255,0,0,0.25)',
+                      borderRadius: '50%',
+                      fontSize: '2rem',
+                      padding: 12,
+                    }}
+                  />
+                }
+                title={'Doanh thu'}
+                value={new Intl.NumberFormat('vi-VN', {
+                  style: 'currency',
+                  currency: 'VND',
+                }).format(brief?.profit)}
               />
-            }
-            title={'Doanh thu'}
-            value={new Intl.NumberFormat('vi-VN', {
-              style: 'currency',
-              currency: 'VND',
-            }).format(700000000)}
-          />
-        </Col>
-      </Row>
-      <br />
-      <Row>
-        <Col span={12} style={{ paddingRight: 10 }}>
-          <LineChart />
-        </Col>
-        <Col span={12} style={{ paddingLeft: 10 }}>
-          <PieChart />
-        </Col>
-      </Row>
-      <Row style={{ marginTop: 30 }}>
-        <Col span={8}>
-          <RecentOrders />
-        </Col>
-        <Col span={16} style={{ paddingLeft: 30 }}>
-          <BarChart />
-        </Col>
-      </Row>
-      <br />
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Col span={12} style={{ paddingRight: 10 }}>
+              <LineChart />
+            </Col>
+            <Col span={12} style={{ paddingLeft: 10 }}>
+              <PieChart />
+            </Col>
+          </Row>
+          <Row style={{ marginTop: 30 }}>
+            <Col span={8}>
+              <RecentOrders />
+            </Col>
+            <Col span={16} style={{ paddingLeft: 30 }}>
+              <BarChart />
+            </Col>
+          </Row>
+          <br />
+        </>
+      )}
     </>
   );
 }
