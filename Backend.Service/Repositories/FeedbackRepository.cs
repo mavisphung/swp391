bcada8a15fa4cc558a6fcd61966b498bf4405119
Huy Phung <huypc2410@gmail.com>
2023-03-15 20:21:23 +0700

    [ADD] Add api get profits by date and api get history orders for user

---
+++ Backend.Service/Repositories/FeedbackRepository.cs
@@ -0,0 +1,14 @@
+﻿using Backend.Service.Entities;
+using Backend.Service.Repositories.IRepositories;
+
+namespace Backend.Service.Repositories
+{
+    public class FeedbackRepository : GeneralRepository<Feedback>, IFeedbackRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public FeedbackRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
